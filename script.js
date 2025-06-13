d8f294b9f62b69d40bb174b9c7cf7fb73435d46b
Adittyaaa-codes <aditya.mallick.1512@gmail.com>
2025-06-13 13:04:56 +0530

    Fully Completed

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+
+        if (navLinks.classList.contains('active')) {
+            document.body.classList.add('no-scroll');
+        } else {
+            document.body.classList.remove('no-scroll');
+        }
+    });
+
+    document.querySelectorAll('#nav-links a').forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.classList.remove('active');
+            document.body.classList.remove('no-scroll');
+        });
+    });
+});
+
 
 
 
